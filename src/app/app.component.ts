42af1c5c1e61f4513c08b8734700783e35895e4d
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-08-05 18:42:29 +0200

    code cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,9 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-
-import { TranslateService } from "ng2-translate";
-
-import { AppConstantsService } from "./core/core.module";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  protected translate: TranslateService;
-  protected appConstants: AppConstantsService;
-
-  constructor(translateService: TranslateService,
-              appConstantsService: AppConstantsService) {
-    this.translate = translateService;
-    this.appConstants = appConstantsService;
-
-    this.translate.setDefaultLang(this.appConstants.Languages.DEFAULT_LANGUAGE);
-    this.translate.use(this.appConstants.Languages.DEFAULT_LANGUAGE);
-  }
-
-  public ngOnInit(): void {
-  }
-
-  public ngOnDestroy(): void {
-  }
+export class AppComponent {
 }
