7cccc075ead3ef1146c97fee72fff932b3670f0e
Maurizio Cescon <Maurizio.CESCON@ext.ec.europa.eu>
2017-11-13 16:15:03 +0100

    minor changes

--- mock-server/index.js
+++ mock-server/index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./mock-server/db.json");
 const middlewares = jsonServer.defaults({
-  // static: "dist",
+  static: "dist",
 });
 
 // Add middlewares
