4bd8981f2bfaa35ab34c7fbf5bc90ed417903588
Maurizio Cescon <maurizio.cescon@gmail.com>
2018-08-01 18:40:41 +0200

    minor changes

--- src/app/shared/text-filter/text-filter.component.spec.ts
+++ src/app/shared/text-filter/text-filter.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
