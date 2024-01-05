8e7226590af7219771c4b5d1bc213e320c661c13
Maurizio Cescon <maurizio.cescon@gmail.com>
2024-01-05 09:21:16 +0100

    chore: removal of charts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'albums', loadChildren: () => import('./albums/albums.routes') },
-  { path: 'chart', loadChildren: () => import('./chart/chart.routes') },
   { path: 'users', loadChildren: () => import('./users/users.routes') },
   { path: '**', redirectTo: '/users' },
 ];
--- src/app/chart/chart.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-
-import { ChartComponent } from './chart.component';
-
-export default [
-  { path: '', component: ChartComponent },
-] satisfies Route[];
